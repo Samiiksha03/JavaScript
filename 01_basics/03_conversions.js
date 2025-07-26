9bb19ff96ddba19eb72d2e8cf8eebf1a119aefec
Samiiksha03 <140168607+Samiiksha03@users.noreply.github.com>
2025-07-26 09:58:20 +0000

    Conversions done

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,31 @@
+// Conersion Operations
+// Basically these are used to convert a DT into another DT(Ex. number to string)
+// Syntax is just the required DT the variable
+
+// Conversion of number DT to String DT
+let num=33
+console.log(num);
+console.log(typeof num);
+let stringnum= String(num)
+console.log(typeof stringnum);
+
+// Conversion of string DT to number DT
+let x="33abc"
+console.log(x);
+console.log(typeof x);
+let xx= Number(x)
+console.log(typeof xx);
+console.log(xx);
+
+//Conversion of string DT to Boolean DT
+let y="abc"
+console.log(y);
+console.log(typeof y);
+let yy= Boolean(y)
+console.log(typeof yy);
+console.log(yy);
+// 1 => true; 0 => false
+// "Samiksha" => true
+// "" => false
+
+
